de29bed66ad7a123b7aa7faad94d54c992a90aef
Oyindamola Subair <oyindamola.subair@andela.com>
2019-07-12 01:43:53 -0700

    save user endpoint

--- app/controllers/ApiController.scala
+++ app/controllers/ApiController.scala
@@ -1,14 +1,32 @@
 package controllers
 
 import auth.AuthAction
+import database.UserRepository
 import javax.inject.{Inject, Singleton}
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.i18n.I18nSupport
 import play.api.mvc.{AbstractController, ControllerComponents}
 import play.api.libs.json.Json
 import repositories.DataRepository
 
+import scala.concurrent.ExecutionContext
+
+
 @Singleton
-class ApiController @Inject()(cc: ControllerComponents, dataRepository: DataRepository, authAction: AuthAction)
-  extends AbstractController(cc) {
+class ApiController @Inject()(cc: ControllerComponents, dataRepository: DataRepository, userRepo: UserRepository, authAction: AuthAction)(implicit ec: ExecutionContext)
+  extends AbstractController(cc) with I18nSupport  {
+
+//  val Home = Redirect(routes.UserController.list(0, 2, ""))
+
+  val userForm: Form[CreateUserForm] = Form {
+    mapping(
+      "name" -> nonEmptyText,
+      "password" -> nonEmptyText,
+      "confirmPassword" -> nonEmptyText
+    )(CreateUserForm.apply)(CreateUserForm.unapply)
+  }
+
 
   // Create a simple 'ping' endpoint for now, so that we
   // can get up and running with a basic implementation
@@ -17,7 +35,6 @@ class ApiController @Inject()(cc: ControllerComponents, dataRepository: DataRepo
   }
 
   def getPost(postId: Int) = authAction { implicit request =>
-    println("got here")
     dataRepository.getPost(postId) map { post =>
 
       Ok(Json.toJson(post))
@@ -27,6 +44,11 @@ class ApiController @Inject()(cc: ControllerComponents, dataRepository: DataRepo
   def getComments(postId: Int) = authAction { implicit request =>
 
     Ok(Json.toJson(dataRepository.getComments(postId)))
+  }
+
+  def getAllUser = Action.async { implicit request =>
+    val users = userRepo.getAllUser
+    users.map(u => Ok(Json.toJson(u)))
   }
 
 }
--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -2,23 +2,39 @@ package controllers
 
 import database.UserRepository
 import javax.inject._
+import models.UserEntity
 import play.api.data.Form
 import play.api.data.Forms._
 import play.api.data.validation.Constraints._
 import play.api.i18n._
+import play.api.libs.json.Json
 import play.api.mvc._
+import views.html
+import play.libs.crypto._
 
 import scala.concurrent.ExecutionContext
 
-class UserController @Inject()(userRepo: UserRepository, cc: MessagesControllerComponents)(implicit ec: ExecutionContext) extends MessagesAbstractController(cc) {
+class UserController @Inject()(userRepo: UserRepository, cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc) with I18nSupport {
 
   val userForm: Form[CreateUserForm] = Form {
     mapping(
-      "name" -> nonEmptyText
+      "name" -> nonEmptyText,
+      "password" -> nonEmptyText,
+      "confirmPassword" -> nonEmptyText
     )(CreateUserForm.apply)(CreateUserForm.unapply)
   }
 
-//  def index = Action { implicit request ⇒
-//    Ok(views.html.index(userForm))
-//  }
+
+  def getAllUser = Action.async { implicit request =>
+    val users = userRepo.getAllUser
+    users.map(u => Ok(Json.toJson(u)))
+  }
+
+  def saveUser = Action.async { implicit request =>
+    val json = request.body.asJson.get
+    val user = json.as[CreateUserForm]
+    for {
+      _ <- userRepo.create(user.name, user.password)
+    } yield Ok
+  }
 }
